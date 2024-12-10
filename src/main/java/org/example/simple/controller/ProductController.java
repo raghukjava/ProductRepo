9141d31f6b44d85af770853e0803cb0d08eef77d
raghu333 <raghu333@gmail.com>
2024-12-10 20:32:03 +0530

    Commit 2

--- src/main/java/org/example/simple/controller/ProductController.java
+++ src/main/java/org/example/simple/controller/ProductController.java
@@ -10,6 +10,8 @@ public class ProductController {
     @GetMapping("/hello")
     public void helloWorld() {
         System.out.println(".............");
+        System.out.println("commit 2 ");
+
 
     }
 }
