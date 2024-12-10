c5b37b71d9e22c2d38b2436da62435609fcf42b0
raghu333 <raghu333@gmail.com>
2024-12-10 19:55:55 +0530

    initial commit massage 7:55

---
+++ src/main/java/org/example/simple/controller/ProductController.java
@@ -0,0 +1,15 @@
+package org.example.simple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hi")
+public class ProductController {
+    @GetMapping("/hello")
+    public void helloWorld() {
+        System.out.println(".............");
+
+    }
+}
